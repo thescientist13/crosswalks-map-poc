b10d6602a0f0258e902e85fe15e13f1061f554d6
Owen Buckley <owen.buckley@analogstudios.net>
2017-09-17 22:52:20 -0400

    console cleanup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import L from 'leaflet';
 
-console.log('leaflet here!', L); // eslint-disable-line
-console.log('geolocation!', navigator.geolocation); // eslint-disable-line
-
-navigator.geolocation.getCurrentPosition((obj) => { // eslint-disable-line
-  console.log('current position callback', obj); // eslint-disable-line
-  console.log('current position callback', obj.coords.latitude); // eslint-disable-line
+navigator.geolocation.getCurrentPosition((obj) => {
   document.getElementById('root').innerHTML = obj.coords.latitude + ' * ' + obj.coords.longitude;
   document.getElementById('time').innerHTML = obj.timestamp;
 });
