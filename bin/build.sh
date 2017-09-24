34c97f3d9c3268d6d6277623cf8fa941ce0f2226
Owen Buckley <obuckley@kenzan.com>
2017-09-23 20:00:29 -0400

    setup circle (#8)
    * setup circle
    
    * test ci commit
    
    * test ci commit
    
    * fixed permissions
    
    * fixed linting error

---
+++ bin/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "node version"
+node -v
+echo "npm version"
+npm -v
+echo "yarn version"
+yarn --version
+
+echo "install dependencies..."
+rm -rf node_modules/ > /dev/null 2>&1
+yarn install
+
+# project setup
+yarn run setup
+
+# build
+yarn run build
