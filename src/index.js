bd8d2f68666648911433e34a3701dbda79077617
Owen Buckley <owen.buckley@analogstudios.net>
2017-09-24 12:39:05 -0400

    refactor

--- src/index.js
+++ src/index.js
@@ -43,9 +43,8 @@ function getUserCoordinates() {
   }
 }
 
-function createMap(latitude, longitude) {
-  const coordsArray = [latitude, longitude];
-  const map = L.map('map').setView(coordsArray, 13);
+function createMap(currentLatitude, currentLongitude) {
+  const map = L.map('map').setView([currentLatitude, currentLongitude], 13);
 
   L.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${config.mapboxApiKey}`, {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>', // eslint-disable-line
